f671b3c03ba2cc75705da2edcd1bac1eb2187b85
imalKesara <imalkesara2001@gmail.com>
2026-05-14 21:19:51 +0530

    postinstall script for generate prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,8 +2,9 @@ import 'dotenv/config';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { PrismaLibSql } from '@prisma/adapter-libsql';
 import { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } from '$env/static/private';
+import { PrismaClient } from '@prisma/client';
 
-import { PrismaClient } from '../../generated/prisma/client';
+// import { PrismaClient } from '../../generated/prisma/client';
 const connectionString = `${process.env.DATABASE_URL}`;
 
 // const adapter = new PrismaBetterSqlite3({ url: connectionString });
