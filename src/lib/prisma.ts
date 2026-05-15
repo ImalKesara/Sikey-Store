d4a31a5128ade45f8f20c896cf074c1c7da64e99
imalKesara <imalkesara2001@gmail.com>
2026-05-15 17:57:34 +0530

    fixed prisma client issue

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,8 @@
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { PrismaLibSql } from '@prisma/adapter-libsql';
 import { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } from '$env/static/private';
-import { PrismaClient } from '../../generated/prisma/client';
+// import { PrismaClient } from '../../node_modules/@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 // const connectionString = `${process.env.DATABASE_URL}`;
 // const adapter = new PrismaBetterSqlite3({ url: connectionString });
